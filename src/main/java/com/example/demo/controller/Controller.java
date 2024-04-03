0361e92add8188d6c701117f9b569d5ec9983876
Sandhana-Krishnan-S <sandhanakrishnansakthivel@gmail.com>
2024-04-03 21:19:28 +0530

    Day - 5 learned to use more of the methods to post and put delete etc ...

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,32 +1,42 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.model.User;
 import com.example.demo.repo.UserRepo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 public class Controller {
+
 	@Autowired
 	private  UserRepo user;
-	
+
 	@GetMapping("/")
 	public List<User> getAll() {
 		return user.findAll();
 	}
-	
+
 	@PostMapping("/setUser")
 	public User newUser(@RequestBody User newStudent) {
-		
+
 		return user.save(newStudent);
 	}
-	
+	@PutMapping("put/{id}")
+	public ResponseEntity<User> putMethod(@PathVariable long id , @RequestBody User editUser) {
+		Optional<User> existingUser = user.findById(id);
+		if(existingUser.isPresent()) {
+			User val = existingUser.get();
+			val.setMark(editUser.getMark());
+			user.save(val);
+			return new ResponseEntity<>(val , HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
 }
